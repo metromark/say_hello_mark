210310823d081cac0dfdb5838874768728dae0a6
Mark Toledo <mptoledo13@gmail.com>
2024-02-04 19:35:11 +0800

    Change to 0.13

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.12",
+  version="0.13",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
