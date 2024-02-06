1c9560559fd5c1166d3ecd742afca140b9c9dcdc
Mark Toledo <mark@onebyzero.ai>
2024-02-07 07:59:57 +0800

    change NPM TOKEN to Env secrets staging

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.25",
+  version="0.26",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
