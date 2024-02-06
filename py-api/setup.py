f0b58190b0d155fbbc1db6c5139d386ee72f8dfc
Mark Toledo <mark@onebyzero.ai>
2024-02-07 07:04:30 +0800

    add diff check to npm builds

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.20",
+  version="0.21",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
