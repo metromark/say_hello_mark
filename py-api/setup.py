ed6eb2c66c2398baffcb9d365753ea310660449b
Mark Toledo <mark@onebyzero.ai>
2024-02-07 07:14:30 +0800

    add creds the deploy jobs

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.21",
+  version="0.22",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
