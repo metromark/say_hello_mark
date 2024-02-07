74cd1f2d0355968b3c1e5623cc9f4df14cfca8de
Mark Toledo <mark@onebyzero.ai>
2024-02-07 08:21:48 +0800

    AWS Creds + Node auth token + version

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.30",
+  version="0.31",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
