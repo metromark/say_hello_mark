ab7053c405767af2a29668a283a438014078ffe2
Mark Toledo <mark@onebyzero.ai>
2024-02-07 08:19:14 +0800

    Change to env.NPM_TOKEN

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.29",
+  version="0.30",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
