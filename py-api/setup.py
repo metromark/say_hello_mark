ed6bd0ec05ddd41703fe3a72e40e8ce9b06b6e65
Mark Toledo <mark@onebyzero.ai>
2024-02-07 15:07:11 +0800

    Reupload 10

--- py-api/setup.py
+++ py-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="say_hello_mark",
-  version="0.34",
+  version="0.35",
   author="Mark Toledo",
   author_email="mark@onebyzero.ai",
   # url="www.google.com",
